9e3a3d67dbc13ff05d9f84c1c17f7c7586e979c4
AdrianM10 <adrianmudzwiti@gmail.com>
2025-07-05 08:41:59 -0400

    Test if disgust is sdominant emotion

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -5,11 +5,15 @@ class TestEmotionDetection(unittest.TestCase):
     def test_emotion_detector(self):
         # Test if joy is dominant emotion
         response_joy = emotion_detector("I am glad this happened")
-        self.assertEqual(response_joy['dominant_emotion'], 'joy')
+        self.assertEqual(response_joy["dominant_emotion"], "joy")
 
         # Test if anger is dominant emotion
         response_anger = emotion_detector("I am really mad about this")
-        self.assertEqual(response_anger['dominant_emotion'], 'anger')
+        self.assertEqual(response_anger["dominant_emotion"], "anger")
+
+        # Test if disgust is sdominant emotion
+        response_disgust = emotion_detector("I feel disgusted just hearing about this")
+        self.assertEqual(response_disgust["dominant_emotion"], "disgust")
        
       
 if __name__ == '__main__':
